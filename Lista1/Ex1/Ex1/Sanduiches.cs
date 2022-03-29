95cd0071b45ce6221f1457f471b4a7d5c6b4058e
adriele <adrieleestefania@hotmail.com>
2022-03-29 01:45:08 -0300

    commit inicial

---
+++ Lista1/Ex1/Ex1/Sanduiches.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex1
+{
+    internal class Sanduiches
+    {
+        public abstract class Sanduiches : Ingredientes
+        {
+            public Ingredientes orderIngredientes(string type)
+            {
+                Ingredientes ingredientes;
+
+                ingredientes = createIngredientes(type);
+                ingredientes.prepare(); //preparar
+                ingredientes.bake(); //mortar
+                ingredientes.cut(); //cortar
+                ingredientes.box(); //colocar na caixa
+                return ingredientes;
+            }
+
+            protected abstract Ingredientes createIngredientes(string type);
+        }
+    }
+}
