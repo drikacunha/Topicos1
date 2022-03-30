1ea50cf8bccb690e683fd57293907bb3e9e78536
adriele <adrieleestefania@hotmail.com>
2022-03-30 05:06:02 -0300

    Commit

--- Lista2/Ex2/Ex2/obj/Debug/net6.0/Ex2.AssemblyInfo.cs
+++ Lista2/Ex2/Ex2/obj/Debug/net6.0/Ex2.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Ex3")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Ex2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Ex3")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Ex3")]
+[assembly: System.Reflection.AssemblyProductAttribute("Ex2")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Ex2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
