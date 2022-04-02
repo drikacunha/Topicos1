7ee71e3e8c60af9717b02b91d01607583a64c0bf
adriele <adrieleestefania@hotmail.com>
2022-04-02 03:06:04 -0300

    Commit

--- Lista1/Ex2/Ex2/Program.cs
+++ Lista1/Ex2/Ex2/Program.cs
@@ -1,20 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Celular celular = new Motorola();
 
-using Ex2;
-
-Console.WriteLine("Hello, World!");
-
-
-CelularFactory factory;
-
-factory = new Nokia();
-factory = new Motorola();
-factory = new Apple();
-
-Celular celular = factory.CreateCelular(TipoCelular.Fabrica);
-
-factory.MontarCelular(TipoCelular.Fabrica);
-
-factory = new Nokia();
-factory = new Motorola();
-factory = new Apple();
+        }
+    }
+}
