6a45c105c865c733500909c61f4de08d580f460f
adriele <adrieleestefania@hotmail.com>
2022-04-01 21:17:39 -0300

    commit

---
+++ Lista1/Ex2/Ex2/CameraNokia.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex2
+{
+    internal class CameraNokia : TipoCamera
+    {
+        public CameraNokia()
+        {
+            Descricao = "Camera Nokia";
+        }
+    }
+}
