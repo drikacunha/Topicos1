0b78dd962f3eda7e5ba0784f52748863c58398b3
adriele <adrieleestefania@hotmail.com>
2022-05-20 20:46:47 -0300

    Commit

---
+++ Lista3/Ex2/Ex2/BubbleSort.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex2
+{
+    public class BubbleSort
+    {
+        public void sort(double[] a)
+        {
+            bubbleSort(a, a.Length);
+        }
+
+        public void bubbleSort(double[] a, int n)
+        {
+            for (int i = n - 1; i > 0; i--)
+            {
+                for (int j = 0; j < i; j++)
+                    if (a[j] > a[j + 1])
+                    {
+                        double temp = a[j];
+                        a[j] = a[j + 1];
+                        a[j + 1] = temp;
+                    }
+            }
+        }
+    }
+}
